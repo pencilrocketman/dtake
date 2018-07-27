45fc22512e4cf1369c1458cbbc39c735f1f25a22
Masayuki Murata <sfcmurata@gmail.com>
2018-07-27 10:40:25 +0900

    tmp

--- lib/dtake/cli.rb
+++ lib/dtake/cli.rb
@@ -6,20 +6,35 @@ require 'thor'
 module Dtake
   class CLI < Thor
 		desc "ls", "show all files on list.html." # コマンドの使用例と、概要
-		def ls()
+		def ls
 			list_url = "https://web.sfc.keio.ac.jp/~takefuji/list.html" 
 			doc = Nokogiri::HTML(open(list_url))
 			doc.inner_text.split("\n").each do |line|
-				next if (line == "") #空行は無視
+				next if line == "" #空行は無視
 				
 				puts line
 			end
 		end
 		
 		desc "d filename", "download file on the server." # コマンドの使用例と、概要
-		def d(filename)
-			command = "wget https://web.sfc.keio.ac.jp/~takefuji/" + filename
-			system(command)
+		def d(name)
+      filenames = []
+      p 'hoge'
+			list_url = "https://web.sfc.keio.ac.jp/~takefuji/list.html" 
+
+			doc = Nokogiri::HTML(open(list_url))
+			doc.inner_text.split("\n").each do |line|
+				next if line == ""
+				filename = line.split(" ").last
+        filenames << filename if filename.match(/(.+)name(.+)/)
+			end
+
+      filenames.each do |filename|
+        wget(name) if filename == name
+        return
+      end
+
+      wget(filenames[0])
 		end
 
 		desc "find query", "search files by filename." # コマンドの使用例と、概要
@@ -28,14 +43,19 @@ module Dtake
 			list_url = "https://web.sfc.keio.ac.jp/~takefuji/list.html" 
 			doc = Nokogiri::HTML(open(list_url))
 			doc.inner_text.split("\n").each do |line|
-				next if (line == "") #空行は無視
+				next if line == "" #空行は無視
 				
 				file_name = line.split(" ").last
-				if(file_name.match(q))
-					puts line
-				end
+			  puts line if file_name.match(/q/)
 			end
 		end
+
+    private
+
+    def wget(name)
+			command = "wget https://web.sfc.keio.ac.jp/~takefuji/" + name
+      system(command)
+    end
 
 	end
 end
